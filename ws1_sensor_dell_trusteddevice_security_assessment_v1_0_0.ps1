a7039cb36df83cdfd02604fa9deebafa9e5825c2
SvenRiebe <86112547+SvenRiebe@users.noreply.github.com>
2021-07-05 15:48:11 +0200

    Add files via upload

---
+++ ws1_sensor_dell_trusteddevice_security_assessment_v1_0_0.ps1
@@ -0,0 +1,18 @@
+﻿##################################################################
+#
+# Name: Sensor Dell TrustedDevice for general result of security assessment
+#
+# Author: Grischa Horst (VMware)
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 06-29-2021
+
+$EventLog = Get-EventLog -LogName DELL -InstanceId 15 -Newest 1 -ErrorAction SilentlyContinue
+
+if($EventLog)
+{
+    return 1
+}
